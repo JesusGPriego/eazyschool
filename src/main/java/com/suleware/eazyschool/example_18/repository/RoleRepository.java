02dd3c6a0b53ebe8f29628f1e92bf7edd4f92571
JesusGPriego <suleware@gmail.com>
2024-12-06 12:30:02 +0100

    hal

--- src/main/java/com/suleware/eazyschool/example_18/repository/RoleRepository.java
+++ src/main/java/com/suleware/eazyschool/example_18/repository/RoleRepository.java
@@ -3,11 +3,13 @@ package com.suleware.eazyschool.example_18.repository;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.suleware.eazyschool.example_18.model.Roles;
 
 @Repository
+@RepositoryRestResource(exported = false)
 public interface RoleRepository extends CrudRepository<Roles, Long> {
   Optional<Roles> getByRoleName(
       String roleName
